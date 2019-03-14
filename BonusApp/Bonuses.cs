54cfe1d3c6cbad6701a0ad7cd4cfddec3f1bfa60
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2019-03-14 08:46:06 +0100

    Add project files.

---
+++ BonusApp/Bonuses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public delegate double BonusProvider(double amount);
+
+    public class Bonuses
+    {
+        //public BonusProvider TenPercent = delegate (double amount)
+        //{
+        //    return amount / 10.0;
+        //};
+
+        public static double TenPercent(double amount)
+        {
+            return amount / 10.0;
+        }
+        //public BonusProvider FlatTwoIfAmountMoreThanFive = delegate (double amount)
+        //{
+        //    return amount > 5.0 ? 2.0 : 0.0;
+        //};
+        public static double FlatTwoIfAmountMoreThanFive(double amount)
+        {
+            return amount > 5.0 ? 2.0 : 0.0;
+        }
+    }
+}
